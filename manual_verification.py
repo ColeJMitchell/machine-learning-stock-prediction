56ef008d10037628ed67aacbf0ca5d674e974c9a
Cole <colejmitchell.cs@gmail.com>
2025-04-16 21:44:45 -0400

    5gb llama model ticker classification. 40 gb model will run overnight to classify the tickers for higher accuracy.

---
+++ manual_verification.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+pd.read_parquet('data/posts-with-tickers.parquet')
+
+# Manual verification of the LLM's output
+
+
+df = pd.read_parquet('data/posts-with-tickers.parquet')
+
+# Iterate through the posts in the parquet file and print the post and its corresponding ticker
+for index, row in df.iterrows():
+    post = row['selftext']
+    ticker = row['ticker']
+    print(f"Post: {post}\nTicker: {ticker}\n")
--- ticker_classifier.py
+++ ticker_classifier.py
@@ -2,19 +2,23 @@ import ollama
 import pandas as pd
 
 # Load the parquet file
-df = pd.read_parquet('data/wallstreetbets-collection.parquet')
+df = pd.read_parquet('data/posts-with-tickers.parquet')
+tickers = []
 
-# Iterate through the posts in the parquet file and 
-with open('data/tickers.txt', 'w') as f:
-    for post in df['selftext']:
-        response = ollama.chat(
-            model='llama3',  
-            messages=[
-            {'role': 'user', 
-            'content': f'what is the stock ticker for this text "{post}" you have to answer in one word with no period. If you cant determine it just ourput None. Also if there is no text provided output None.'}
+# Iterate through the posts and classify their corresponding tickers
+for post in df['selftext']:
+    response = ollama.chat(
+        model='llama3:70b',  
+        messages=[
+        {'role': 'user', 
+        'content': f'what is the stock ticker for this text "{post}" you have to answer in one word with no period. If you cant determine it just ourput None. Also if there is no text provided output None.'}
         ]
-        )
-        if len(response['message']['content']) > 5:
-            response['message']['content'] = 'None'
-        f.write(response['message']['content'] + '\n')
+    )
+    if len(response['message']['content']) > 5:
+        response['message']['content'] = 'None'
+    tickers.append(response['message']['content'])
 
+df['ticker'] = tickers
+
+# Save the updated DataFrame to the parquet file
+df.to_parquet('data/posts-with-tickers.parquet')
