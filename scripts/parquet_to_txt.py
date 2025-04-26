c40ff07951b6d975409745eb3ace6152bec28bd7
ColeJMitchell <colejmitchell.cs@gmail.com>
2025-04-26 11:04:44 -0400

    added date created to text file

--- scripts/parquet_to_txt.py
+++ scripts/parquet_to_txt.py
@@ -5,5 +5,6 @@ df = pd.read_parquet('../data/posts-with-tickers.parquet')
 # Save the DataFrame to a text file
 for index, row in df.iterrows():
     ticker = row['ticker']
+    date = row['created_utc']
     with open('../data/updated_tickers.txt', 'a') as f:
-        f.write(f"{ticker}\n")
+        f.write(f"{ticker} / {date} \n")
